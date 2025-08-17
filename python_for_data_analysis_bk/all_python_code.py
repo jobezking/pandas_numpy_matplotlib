7d0f5464721bcd5420d01f1a7ac067235f2e6711
jobezking <52982865+jobezking@users.noreply.github.com>
2025-08-16 21:37:28 -0400

    start

---
+++ python_for_data_analysis_bk/all_python_code.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import LabelEncoder
+
+#note: chapter 4 is very important for pandas fundamentals
+#note: chapter 5 is very important for data cleaning
+data = {
+    'Name':['Alice', 'Bob', 'Charlie', 'Diana'],
+    'Age':[25, 30, 35, 40],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']
+}
+df = pd.DataFrame(data)
+nums = np.array([10,20,30,40,50])
+print(np.mean(nums))
+
+print(df.info())
+print(df.head())
+df.to_csv('cleaned_data.csv',index=False)
+
+excel_data = pd.ExcelFile('email_logistic_models.xlsx')
+print(excel_data.sheet_names)
+df_excel = excel_data.parse('Stats 2')
+print(df_excel.head())
+df_excel.to_excel('summary_report.xlsx',sheet_name='Summary',index=False)
+
+df2 = pd.read_excel('email_logistic_models.xlsx',sheet_name='Model 2')
+print(df2.head())
+
+#open json file
+with open('a_sample.json','r') as file:
+    data = json.load(file)
+print(data)
+#save to json file
+with open('output.json','w') as file:
+    json.dump(data,file,indent=4)
+#If json data is flat and tabular, pandas can read it directly
+df_json = pd.read_json('output.json')
+df_json.to_json('data.json',orient='records')
+
+#Matplotlib
+df_data = pd.read_csv("sales_data.csv")
+print(df_data.head())
+print(f"Dataset contains {df_data.shape[0]} rows and {df_data.shape[1]} columns.\n")
+df_data.info()
+print("\n")
+df_data.describe()
+print(df_data['CategoryColumn'].describe())
+print(df_data['Region'].value_counts())
+print(df_data.isnull().sum())
+df_data['quantity'].hist(bins=30) 
+plt.title('Sales Distribution')
+plt.xlabel('Sales') 
+plt.ylabel('Frequency') 
+plt.show()
+
+df_cat = pd.DataFrame({'Color': ['red', 'blue', 'green', 'blue', 'red']})
+
+#One-Hot Encoding
+one_hot = pd.get_dummies(df_cat['Color'],prefix='Color')
+
+#Label Encoding
+label_encoder = LabelEncoder()
+df_cat['Color_label'] = label_encoder.fit_transform(df_cat['Color'])
+
+print("One-Hot Encoding:\n", one_hot) 
+print("\nLabel Encoding:\n", df_cat)
+
+
+
+
+def summarize_data(arr):
+    print(f"Mean: {np.mean(arr)}") 
+    print(f"Median: {np.median(arr)}")  
+    print(f"Variance: {np.var(arr)}")  
+    print(f"Standard Deviation: {np.std(arr)}") 
+    print(f"Min: {np.min(arr)}")  
+    print(f"Max: {np.max(arr)}")  
+    print(f"Range: {np.max(arr) - np.min(arr)}")
+    print(f"25th Percentile (Q1): {np.percentile(arr, 25)}")  
+    print(f"75th Percentile (Q3): {np.percentile(arr, 75)}")
+
+arra = np.array([10, 20, 30, 40, 50])
+summarize_data(arra)
